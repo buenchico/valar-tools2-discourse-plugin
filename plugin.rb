1f56a689d327de94744544cda9a98f304f05809b
buenchico <hammer_ortiz@hotmail.com>
2024-03-18 15:53:24 +0000

    empty plugin

--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,20 @@
-# frozen_string_literal: true
+# plugins/your-plugin-name/plugin.rb
 
-# name: discourse-plugin-name
-# about: TODO
-# meta_topic_id: TODO
-# version: 0.0.1
-# authors: Discourse
-# url: TODO
-# required_version: 2.7.0
-
-enabled_site_setting :plugin_name_enabled
-
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
-end
-
-require_relative "lib/my_plugin_module/engine"
+# add the required dependencies
+gem 'net-http-persistent'
 
 after_initialize do
-  # Code which should run after Rails has finished booting
+  require_dependency 'post'
+
+  on(:post_created) do |post|
+    process_post(post) if post.raw.include?("$army.red")
+  end
+
+  on(:post_edited) do |post, _, _|
+    process_post(post) if post.raw.include?("$army.red")
+  end
+
+  def process_post(post)
+    puts "test"
+  end
 end
